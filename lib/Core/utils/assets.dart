f17650ca45c8e36d677ad1851e4130276c193f19
ali4hassan9ali2022 <kasrawyali47@gmail.com>
2025-03-09 16:21:21 +0200

    generate assets images to my_app

---
+++ lib/Core/utils/assets.dart
@@ -0,0 +1,45 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesImage (1)
+  /// assets/images/image (1).png
+  static const String imagesImage1 = "assets/images/image (1).png";
+
+  /// Assets for imagesImage (2)
+  /// assets/images/image (2).png
+  static const String imagesImage2 = "assets/images/image (2).png";
+
+  /// Assets for imagesImage (3)
+  /// assets/images/image (3).png
+  static const String imagesImage3 = "assets/images/image (3).png";
+
+  /// Assets for imagesOnBoardingBackground
+  /// assets/images/on_boarding_background.png
+  static const String imagesOnBoardingBackground = "assets/images/on_boarding_background.png";
+
+  /// Assets for imagesTopSeller1
+  /// assets/images/topSeller1.png
+  static const String imagesTopSeller1 = "assets/images/topSeller1.png";
+
+  /// Assets for imagesTopSeller2
+  /// assets/images/topSeller2.png
+  static const String imagesTopSeller2 = "assets/images/topSeller2.png";
+
+  /// Assets for imagesTopSeller3
+  /// assets/images/topSeller3.png
+  static const String imagesTopSeller3 = "assets/images/topSeller3.png";
+
+  /// Assets for imagesTrending1
+  /// assets/images/trending1.png
+  static const String imagesTrending1 = "assets/images/trending1.png";
+
+  /// Assets for imagesTrending2
+  /// assets/images/trending2.png
+  static const String imagesTrending2 = "assets/images/trending2.png";
+
+  /// Assets for imagesTrending3
+  /// assets/images/trending3.png
+  static const String imagesTrending3 = "assets/images/trending3.png";
+}
+
