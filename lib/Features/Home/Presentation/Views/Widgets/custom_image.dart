0b8370a95cfc001d3284b5328b70c16d51953411
ali4hassan9ali2022 <kasrawyali47@gmail.com>
2025-03-10 17:45:14 +0200

    create custom image

---
+++ lib/Features/Home/Presentation/Views/Widgets/custom_image.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/widgets.dart';
+import 'package:mini_nft_marketplace_app/Core/utils/app_styles.dart';
+import 'package:mini_nft_marketplace_app/Core/utils/assets.dart';
+
+class CustomImage extends StatelessWidget {
+  const CustomImage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        AspectRatio(
+          aspectRatio: 1.95,
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(27),
+              image: DecorationImage(
+                image: AssetImage(Assets.imagesImage1),
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          left: 170,
+          bottom: 9.14,
+          child: Text("Art", style: AppStyles.styleBold20(context)),
+        ),
+      ],
+    );
+  }
+}
--- lib/Features/Home/Presentation/Views/Widgets/home_view_body.dart
+++ lib/Features/Home/Presentation/Views/Widgets/home_view_body.dart
@@ -1,17 +1,11 @@
-import 'package:flutter/widgets.dart';
-
+import 'package:flutter/material.dart';
+import 'package:mini_nft_marketplace_app/Features/Home/Presentation/Views/Widgets/custom_image.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Column(
-        children: [
-          
-        ],
-      ),
-    );
+    return SafeArea(child: Column(children: [CustomImage()]));
   }
 }
